9a24a058039a8e10ccf936dc4b58b5fa402a85a8
Hunter Wang <s092284s@gmail.com>
2024-08-26 20:34:37 +0800

    add share & edit accesses feature

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,12 +1,8 @@
 import CollaborativeRoom from "@/components/CollaborativeRoom";
-import { Editor } from "@/components/editor/Editor";
-import Header from "@/components/Header";
 import { getDocument } from "@/lib/actions/room.actions";
 import { getClerkUsers } from "@/lib/actions/user.actions";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import React from "react";
 
 const Document = async ({ params: { id } }: SearchParamProps) => {
   const clerkUser = await currentUser();
@@ -36,7 +32,7 @@ const Document = async ({ params: { id } }: SearchParamProps) => {
     : "viewer";
 
   return (
-    <main className=" flex w-full flex-col items-center">
+    <main className="flex w-full flex-col items-center">
       <CollaborativeRoom
         roomId={id}
         roomMetadata={room.metadata}
